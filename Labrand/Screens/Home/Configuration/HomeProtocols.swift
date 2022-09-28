085406d5245fe616a12300b857e53563d2c5750a
shokhzodsunnatov <68766751+shokhzodsunnatov@users.noreply.github.com>
2022-09-28 21:35:23 +0500

    Added HomeConfigure

---
+++ Labrand/Screens/Home/Configuration/HomeProtocols.swift
@@ -0,0 +1,28 @@
+//
+//  HomeProtocols.swift
+//  Labrand
+//
+//  Created by Shakhzod on 27/09/22.
+//
+
+import UIKit
+
+//MARK: - ViewController
+protocol HomePresenterOutput: AnyObject {
+    
+}
+
+//MARK: - Interactor
+protocol HomeInteractable: AnyObject {
+    
+}
+
+//MARK: - Presentable
+protocol HomePresentable: AnyObject {
+    
+}
+
+//MARK: - HomeRouting
+protocol HomeRouting: AnyObject {
+    
+}
---
+++ Labrand/Screens/Home/HomePresenter.swift
@@ -0,0 +1,19 @@
+//
+//  HomePresenter.swift
+//  Labrand
+//
+//  Created by Shakhzod on 27/09/22.
+//
+
+import UIKit
+
+class HomePresenter {
+    
+    //MARK: -  Properties
+    var viewController: HomePresenterOutput?
+}
+
+//MARK: - HomePresetable
+extension HomePresenter: HomePresentable {
+    
+}
